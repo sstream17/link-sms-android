f8eb846b755983f79a10519ed43b81b88de67945
Spencer Stream <sprewstream@gmail.com>
2020-03-15 11:32:36 -0500

    Adding a new activity for bottom navigation

---
+++ app/src/main/java/xyz/stream/messenger/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package xyz.stream.messenger.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import xyz.stream.messenger.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.setGraph(R.id.navigation_conversations)
+        navView.setupWithNavController(navController)
+    }
+}
