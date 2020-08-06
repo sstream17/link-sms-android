9b25af6db963b8ab32867b74ed0d89f6293cc1f2
Spencer Stream <sprewstream@gmail.com>
2020-08-06 16:28:41 -0500

    Updating bottom nav icons to change color based on theme

--- app/src/main/java/xyz/stream/messenger/activity/main/MainColorController.kt
+++ app/src/main/java/xyz/stream/messenger/activity/main/MainColorController.kt
@@ -1,10 +1,13 @@
 package xyz.stream.messenger.activity.main
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Build
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat.getColor
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import xyz.stream.messenger.R
 import xyz.stream.messenger.activity.MessengerActivity
 import xyz.stream.messenger.shared.data.Settings
@@ -17,6 +20,7 @@ class MainColorController(private val activity: AppCompatActivity) {
 
     private val toolbar: Toolbar by lazy { activity.findViewById<View>(R.id.toolbar) as Toolbar }
     private val conversationListContainer: View by lazy { activity.findViewById<View>(R.id.conversation_list_container) }
+    private val bottomNav: BottomNavigationView by lazy { activity.findViewById<View>(R.id.nav_view) as BottomNavigationView }
 
     fun colorActivity() {
         ColorUtils.checkBlackBackground(activity)
@@ -35,6 +39,12 @@ class MainColorController(private val activity: AppCompatActivity) {
         toolbar.setBackgroundColor(Settings.mainColorSet.color)
 
         ColorUtils.adjustStatusBarColor(Settings.mainColorSet.color, Settings.mainColorSet.colorDark, activity)
+
+        val states = arrayOf(intArrayOf(android.R.attr.state_selected), intArrayOf(-android.R.attr.state_selected))
+        val colors = intArrayOf(Settings.mainColorSet.color, getColor(activity, R.color.secondaryText))
+        val colorStateList = ColorStateList(states, colors)
+        bottomNav.itemIconTintList = colorStateList
+        bottomNav.itemTextColor = colorStateList
 
         if (Settings.baseTheme == BaseTheme.BLACK) {
             conversationListContainer.setBackgroundColor(Color.BLACK)
